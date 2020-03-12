86d1633b941b51ef82a9f152dcbc3cc4308f10b0
Alex Petrov <olpe@chevron.com>
2020-03-12 11:57:25 -0500

    added solution to binary gap

---
+++ Code/Exercises/BinaryGap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Niipazzo.Exercises
+{
+    public static class BinaryGap
+    {
+        public static int Solve(int n)
+        {
+            var bits = Convert.ToString(n, 2);
+            var groups = bits.Split('1').Skip(1).SkipLast(1).ToList();
+            groups.Add(string.Empty);
+            var gap = groups.Max(x => x.Length);
+            return gap;
+        }
+    }
+}
